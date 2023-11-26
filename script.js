037ce89c621e58665dd0423be00b298716daebed
Md Zahirul Haque <116150226+zahir1206@users.noreply.github.com>
2023-11-27 02:35:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function stringChop(str, size) {
   // your code here
+	if(str===null){
+		return [];
+	}
+	let i=0,ans=[];
+	while(i<=str.length-1){
+		ans.push(str.slice(1,i+size));
+		i=i+size;
+	}
+	return ans;
 }
 
 // Do not change the code below
